242061a4f1e54a5958b3c5504d7d6c519d0cf7cb
RAIZEL <yureasel@gmail.com>
2024-02-05 00:04:30 +0800

    Revert "enter state update animation"
    This reverts commit b449c3db2a94d2742042a3461f0a4444e45dcf03.

--- TicTacToe2/enter-script.js
+++ TicTacToe2/enter-script.js
@@ -2,16 +2,3 @@ document.getElementById('playButton').addEventListener('click', () => {
     // Redirect to the main game page
     window.location.href = 'main-game.html';
 });
-
-
-//FROM: TeenageProgrammer
-
-const bgAnimation = document.getElementById('bgAnimation');
-
-const numberOfColorBoxes = 400;
-
-for (let i = 0; i < numberOfColorBoxes; i++) {
-    const colorBox = document.createElement('div');
-    colorBox.classList.add('colorBox');
-    bgAnimation.append(colorBox)
-}

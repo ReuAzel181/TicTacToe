1910fe570901682db936a2b06b48baa3034f54b8
RAIZEL <yureasel@gmail.com>
2024-02-07 03:36:39 +0800

    Copying all files from update branch

--- TicTacToe2/enter-script.js
+++ TicTacToe2/enter-script.js
@@ -1,18 +1,23 @@
 document.getElementById('playButton').addEventListener('click', () => {
-    // Redirect to the main game page
-    window.location.href = 'main-game.html';
-});
+    // Play the click sound
+    var clickSound = document.getElementById("clickSound");
+    clickSound.play();
 
+    // Delay the redirection by 1 second (1000 milliseconds)
+    setTimeout(() => {
+        // Redirect to the main game page
+        window.location.href = 'main-game.html';
+    }, 1100); // Adjust the delay time as needed
+});
 
 //FROM: TeenageProgrammer
 
 const bgAnimation = document.getElementById('bgAnimation');
 
-const numberOfColorBoxes = 400;
+const numberOfColorBoxes = 120;
 
 for (let i = 0; i < numberOfColorBoxes; i++) {
     const colorBox = document.createElement('div');
     colorBox.classList.add('colorBox');
     bgAnimation.append(colorBox)
 }
-
